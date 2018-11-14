1f1e9fa88c1a67f4b1f96647cbd227e16d17f918
wgallardo <wgallardob@gmail.com>
2018-11-14 12:04:23 +0100

    Errores Modificados

--- QueryConsultasWalterGallardo.sql
+++ QueryConsultasWalterGallardo.sql
@@ -4,33 +4,32 @@ select count(*) from flights;
 
 /*2. Retard promig de sortida i arribada segons l’aeroport origen*/
 
-select b.Airport as AirportOrigen, avg(a.ArrDelay) as ArrivalDelay , avg(a.DepDelay) as DepartureDelay 
-From flights as a inner join usairports as b
-where a.Origin=b.IATA
-group by b.Airport
+select origin , avg(ArrDelay) as ArrivalDelay , avg(DepDelay) as DepartureDelay 
+From flights 
+group by Origin
 
 /* 3.Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen. */
 
-select b.iata as AirportOrigen,a.colYear as Year ,  a.colMonth as Month,
- if(a.ArrDelay >0 , "Retard","") as Estatus
-From flights as a inner join usairports as b
-where a.Origin=b.IATA and a.ArrDelay > 0
-group by b.Airport
+select origin, colYear as Year , colMonth as Month,
+ if(ArrDelay >0 , "Retard","") as Estatus
+From flights a
+where ArrDelay > 0
+group by origin
 
 /*4.Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen
 (mateixa consulta que abans i amb el mateix ordre).
  Però a més, ara volen que en comptes del codi de l’aeroport es mostri el nom de la ciutat. */
  
- select b.city, b.iata as AirportOrigen,a.colYear as Year ,  a.colMonth as Month,
- if(a.ArrDelay >0 , "Retard","") as Estatus
-From flights as a inner join usairports as b
-where a.Origin=b.IATA and a.ArrDelay > 0
+select b.city, b.iata as AirportOrigen,a.colYear as Year ,  a.colMonth as Month,
+if(a.ArrDelay >0 , "Retard","") as Estatus
+From flights as a inner join usairports as b  on a.Origin=b.IATA
+where  a.ArrDelay > 0
 group by b.Airport
  
  /*5.Les companyies amb més vols cancelats. 
  A més, han d’estar ordenades de forma que les companyies amb més cancelacions apareguin les primeres. */
  
- Select a.Description as Companys, sum(b.Cancelled) as VolsCancelled
+ Select a.Description as Companys, count(b.Cancelled) as VolsCancelled
  from carriers as a
  inner join flights as b 
  on a.CarrierCode=b.UniqueCarrier
@@ -42,24 +41,25 @@ group by b.Airport
  
 /*6.L’identificador dels 10 avions que més distància han recorregut fent vols. */
 
-Select a.Description ,b.TailNum, b.Distance
+Select a.Description ,b.FlightNum ,b.Distance
 from flights as  b
 inner join carriers as a
 on a.CarrierCode=b.UniqueCarrier
-order by b.Distance desc
+group by b.FlightNum
 limit 10
+
 
 
 
 /*Companyies amb el seu retard promig només d’aquelles les quals els seus vols arriben
  al seu destí amb un retràs promig major de 10 minuts. */
  
- Select a.Description , avg(b.ArrDelay) as Arrival
+ Select a.Description as Company , avg(b.ArrDelay) as Arrival
 from flights as  b
 inner join carriers as a
 on a.CarrierCode=b.UniqueCarrier
-where b.ArrDelay > 10
 group by a.Description
+having Arrival > 10
 
 
 
